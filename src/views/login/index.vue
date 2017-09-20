7f9ad76b9a7fd1bb9b0fbed95050226282736066
bingzhe <874177815@qq.com>
2017-09-20 12:45:33 +0800

    style

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="login-container">
+    <!-- 背景图片 start -->
+    <div class="bg-img"></div>
+    <!-- 背景图片 end -->
+
+    <div class="login-content clearfix">
+      <!-- login start -->
+      <div class="login-left left"></div>
+      <!-- login end -->
+
+      <!-- 公众号二维码  start-->
+      <div class="login-right left">
+        <div class="neweat-img">
+          <img src="" alt="新吃货公众号">
+        </div>
+        <p class="text">扫一扫，关注新吃货公众号</p>
+      </div>
+      <!-- 公众号二维码 end -->
+    </div>
+
   </div>
 </template>
 
@@ -10,13 +29,36 @@ export default {
 </script>
 
 <style scoped lang="scss">
-$or: #ccc;
+@import '../../styles/mixin.scss';
 
-.login-container{
-  position: absolute;
-  width:100%;
+$blue: rgb(98, 138, 233);
+
+.login-container {
+  position: relative;
+  width: 100%;
   height: 100%;
-  background-color: $or;
+  height: 100vh;
+
+  .bg-img {
+    background: $blue;
+    width: 100%;
+    height: 50vh;
+  }
 }
 
+.login-container .login-content{
+    position: fixed;
+    @include wh(260px, 350px);
+    border: 1px solid #ccc;
+    border-top: 5px solid rgb(90,140,255);
+    border-radius: 0 0 10px 10px;
+    
+    .login-left{
+      @include wh(62.5%, 100%);
+    }
+    .login-right{
+      @include wh(37.5%, 100%);
+      border-left: 1px solid rgb(244,244,244);
+    }
+}
 </style>
